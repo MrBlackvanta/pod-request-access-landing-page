afde9ea78d0b607de3cddda0cf8070d9e7305d84
Abdelrhman Abdelaal <aabdelaal@appswave.io>
2026-09-11 16:54:24 +0300

    Add Hero component and update Home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { PodLogo } from "@/components/icons";
+import { Hero } from "@/views/home";
+
 export default function Home() {
-  return <main className="min-h-dvh" />;
+  return (
+    <div className="grid min-h-dvh grid-rows-[1fr_auto_1fr]">
+      <div className="row-start-2 px-9 md:px-11">
+        <div className="mx-auto w-full max-w-280">
+          <header className="flex justify-center md:justify-start">
+            <PodLogo />
+            <span className="sr-only">pod</span>
+          </header>
+          <Hero />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/views/home/hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <main className="md:bg-page mt-14 text-center md:mt-26 md:max-w-148 md:pt-22 md:text-left lg:max-w-184">
+      <h1 className="text-display text-green md:text-display-lg uppercase md:max-w-132 lg:max-w-none">
+        Publish your podcasts{" "}
+        <span className="block text-white">everywhere.</span>
+      </h1>
+      <p className="text-lead text-copy md:text-lead-lg mt-4 md:mt-6 md:max-w-112">
+        Upload your audio to pod with a single click. We&rsquo;ll then
+        distribute your podcast to Spotify, Apple Podcasts, Google Podcasts,
+        Pocket Casts and more!
+      </p>
+    </main>
+  );
+}
